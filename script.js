a6c8ade197c1297cb792f0e64bb01d3a0d5b5134
akshitgajera1013 <akshitgajera22@gmail.com>
2026-07-27 19:23:35 +0530

    'Adding_BackendAPI'

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             // Note: Update URL if hosted elsewhere. CORS must be configured on FastAPI server.
-            const response = await fetch('http://127.0.0.1:8000/predict', {
+            const response = await fetch('https://mental-health-prediction-intelligence-2.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
